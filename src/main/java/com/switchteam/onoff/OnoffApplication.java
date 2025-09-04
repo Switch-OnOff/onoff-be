7514753f9ab87de107c452883b3a704d1d5d2ff8
이학준 <dlgkrwns213@naver.com>
2025-09-05 01:10:36 +0900

    프로젝트 전체 교체

---
+++ src/main/java/com/switchteam/onoff/OnoffApplication.java
@@ -0,0 +1,13 @@
+package com.switchteam.onoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnoffApplication.class, args);
+	}
+
+}
