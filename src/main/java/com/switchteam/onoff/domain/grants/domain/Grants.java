6a5f151b5b9d13fef2d346c747f4eeb177c3b39d
yyujin1231 <118620724+yyujin1231@users.noreply.github.com>
2025-09-10 03:42:22 +0900

    ⚡️feat: entity 추가

---
+++ src/main/java/com/switchteam/onoff/domain/grants/domain/Grants.java
@@ -0,0 +1,43 @@
+package com.switchteam.onoff.domain.grants.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "grants")
+@Getter
+@NoArgsConstructor
+public class Grants {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "service_id")
+    private Long serviceId;
+
+    @Column(name = "service_name", nullable = false, length = 255)
+    private String serviceName;
+
+    @Column(length = 255)
+    private String industry;
+
+    @Column(length = 255)
+    private String location;
+
+    @Column(name = "service_status", length = 255)
+    private String serviceStatus;
+
+    @Lob
+    @Column(name = "selection_criteria")
+    private String selectionCriteria;
+
+    @Lob
+    @Column(name = "required_documents")
+    private String requiredDocuments;
+
+    @Column(name = "service_link", length = 1024)
+    private String serviceLink;
+
+
+
+}
