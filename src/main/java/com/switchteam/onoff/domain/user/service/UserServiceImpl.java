5146b37e8dba4240715dbb844f55c878c920fde1
이학준 <dlgkrwns213@naver.com>
2025-09-10 22:57:47 +0900

    feat: UserService 선언 및 구현

---
+++ src/main/java/com/switchteam/onoff/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.switchteam.onoff.domain.user.service;
+
+import com.switchteam.onoff.domain.user.domain.User;
+import com.switchteam.onoff.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
