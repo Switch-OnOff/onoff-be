a994a76c7eb6fa0018c5d86468134b2834a2d718
이학준 <dlgkrwns213@naver.com>
2025-09-11 00:16:26 +0900

    feat: 판매자, 구매자로 방 전체 찾기 기능 추가

--- src/main/java/com/switchteam/onoff/domain/chat/repsoitory/ChatRoomRepository.java
+++ src/main/java/com/switchteam/onoff/domain/chat/repsoitory/ChatRoomRepository.java
@@ -3,5 +3,9 @@ package com.switchteam.onoff.domain.chat.repsoitory;
 import com.switchteam.onoff.domain.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    List<ChatRoom> findAllByBuyerIdOrderByCreatedAtDesc(Long buyerId);
+    List<ChatRoom> findAllBySellerIdOrderByCreatedAtDesc(Long sellerId);
 }
