ed4d6265db52d5ac65c2890cda140b78dd5e083a
이학준 <dlgkrwns213@naver.com>
2025-09-11 00:21:50 +0900

    feat: ChatRoomRestController 추가

---
+++ src/main/java/com/switchteam/onoff/domain/chat/controller/ChatRoomRestController.java
@@ -0,0 +1,31 @@
+package com.switchteam.onoff.domain.chat.controller;
+
+import com.switchteam.onoff.domain.chat.dto.ChatRoomResponseDto;
+import com.switchteam.onoff.domain.chat.service.ChatRoomRoleService;
+import com.switchteam.onoff.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/chat/rooms")
+@RequiredArgsConstructor
+public class ChatRoomRestController {
+
+    private final ChatRoomRoleService chatRoomRoleService;
+
+    /**
+     * 주어진 사용자 ID에 해당하는 모든 채팅방을 조회합니다.
+     *
+     * @param userId 사용자의 고유 식별자
+     * @return 해당 사용자가 참여한 모든 채팅방의 목록
+     */
+    @GetMapping("/{userId}")
+    public List<ChatRoomResponseDto> getAllRooms(@PathVariable Long userId) {
+        return chatRoomRoleService.getAllRoomByUserId(userId);
+    }
+}
