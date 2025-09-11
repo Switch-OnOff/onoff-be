7fbd3611029b682d01ef6ecd71e8d4e922be4eb2
이학준 <dlgkrwns213@naver.com>
2025-09-12 02:13:18 +0900

    feat: 에러 코드 및 성공 코드 추가

--- src/main/java/com/switchteam/onoff/global/exception/ErrorCode.java
+++ src/main/java/com/switchteam/onoff/global/exception/ErrorCode.java
@@ -33,7 +33,8 @@ public enum ErrorCode {
     CHATROOM_NOT_FOUND(1020, "채팅방을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     IMAGE_TRANSPORT_ERROR(1021, "이미지 전송에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     OPENAI_ANALYSIS_ERROR(1022, "OpenAI 분석에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    PROPERTY_NOT_FOUND(1023, "매물 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+    PROPERTY_NOT_FOUND(1023, "매물 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    AI_GENERATE_ERROR(1024, "AI 생성에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final int code;
     private final String message;
