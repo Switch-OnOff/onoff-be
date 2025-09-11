df0f207494983aeaa57e614b445f16933a97cb3e
이학준 <dlgkrwns213@naver.com>
2025-09-11 16:52:32 +0900

    feat: openai api 관련 설정 추가

---
+++ src/main/java/com/switchteam/onoff/global/config/OpenAiConfig.java
@@ -0,0 +1,19 @@
+package com.switchteam.onoff.global.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class OpenAiConfig {
+
+    @Value("${openai.api.key}")
+    private String openAiApiKey;
+
+    @Value("${openai.api.url}")
+    private String openAiApiUrl;
+
+    @Value("${openai.api.model}")
+    private String openAiApiModel;
+}
