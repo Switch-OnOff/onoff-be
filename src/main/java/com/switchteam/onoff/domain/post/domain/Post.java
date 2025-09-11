545da86f9575bc4940a3570a569cc80b4c88efc3
이학준 <dlgkrwns213@naver.com>
2025-09-11 12:21:28 +0900

    feat: Post entity 선언

---
+++ src/main/java/com/switchteam/onoff/domain/post/domain/Post.java
@@ -0,0 +1,61 @@
+package com.switchteam.onoff.domain.post.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.switchteam.onoff.domain.user.domain.User;
+import com.switchteam.onoff.global.exception.CustomException;
+import com.switchteam.onoff.global.exception.ErrorCode;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(columnDefinition = "TEXT")
+    private String analysisResult;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt;
+
+    // Post : PostImage = 1 : N
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<PostImage> images = new ArrayList<>();
+
+    public void addImage(PostImage image) {
+        if (images.size() > 5) {
+            throw new CustomException(ErrorCode.IMAGE_LIMIT_EXCEEDED);
+        }
+        images.add(image);
+        image.setPost(this);
+    }
+
+    // 개별 이미지 삭제
+    public void removeImage(PostImage image) {
+        images.remove(image);
+        image.setPost(null);
+    }
+}
