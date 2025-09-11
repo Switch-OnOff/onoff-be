c6e26ec81ee37da8f80f42757125aee80cbdfe61
이학준 <dlgkrwns213@naver.com>
2025-09-11 12:21:11 +0900

    feat: PostRequestDto 선언

---
+++ src/main/java/com/switchteam/onoff/domain/post/dto/PostRequestDto.java
@@ -0,0 +1,15 @@
+package com.switchteam.onoff.domain.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class PostRequestDto {
+    private String content;
+    private List<MultipartFile> images = new ArrayList<>();
+}
